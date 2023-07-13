38cc305995ca248f33f0f2b1994f137b19d43876
Herman Zhao | 赵华男 <39041821+hermanzhaozzzz@users.noreply.github.com>
2023-07-14 00:22:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+cd $HOME
+rm -rf .vimbak .vimrcbak
 mv .vim .vimbak &> /dev/null
 mv .vimrc .vimrcbak &> /dev/null
 git clone --recursive --depth=1 https://github.com/hermanzhaozzzz/vim-for-coding.git $HOME/.vim
-cd $HOME
+
 ln -s .vim/vimrc .vimrc
 echo 'success! enjoy it!'
 exit 0
